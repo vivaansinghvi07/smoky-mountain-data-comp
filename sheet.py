4a925aea20bd4d9896f4ed6845762b21d1e39220
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-27 16:03:57 -0400

    updated instructions

--- sheet.py
+++ sheet.py
@@ -1,9 +1,29 @@
 import openpyxl
 
+"""  treatment, block, row,  position, and genotype """
+
+EXAMPLE_DATA = [
+    {
+        'filename': 'example1.png', 
+        'treatment': 'C', 
+        'block': '1', 
+        'row': '54', 
+        'position': '12', 
+        'genotype': 'BESC-4590_LM'
+    },
+    {
+        'filename': 'example2.png', 
+        'treatment': 'D', 
+        'block': '2', 
+        'row': '23', 
+        'position': '10', 
+        'genotype': 'BESC-4230_LM'
+    }
+]
+
 def make_sheet():
-    # gets rows from varrun - assume the list of rows are in this format
-    # [['item', 'item'], ['item', 'item']]
+    # gets rows from varrun - assume the list of rows are in the format in EXAMPLE DATA
 
     # todo: 
-    # create a sheet and write each row of the list in the sheet
+    # create a sheet and write each row of EXAMPLE_DATA in the sheet
     # save the sheet
