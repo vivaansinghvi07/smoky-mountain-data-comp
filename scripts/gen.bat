da8363f73916ec96a8e5234a7352ad04b142efcb
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-29 12:45:22 -0400

    tuhyuhyuhyuhyuhyuhyuhyuhyuh

--- scripts/gen.bat
+++
@@ -1,4 +0,0 @@
-RMDIR /S /Q synthetic-data
-MKDIR synthetic-data synthetic-data\images
-ECHO '*' > synthetic-data\.gitignore
-python3 generate_dataset.py
--- scripts/gen.sh
+++
@@ -1,4 +0,0 @@
-rm -rf synthetic-data
-mkdir synthetic-data synthetic-data/images
-echo '*' > synthetic-data/.gitignore
-python3 generate_dataset.py
