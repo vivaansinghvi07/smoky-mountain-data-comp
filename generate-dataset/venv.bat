5968179e93fb8499fb09448e473a0241f3b1bcea
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-29 09:36:06 -0400

    script update

--- generate-dataset/venv.bat
+++ generate-dataset/venv.bat
@@ -6,14 +6,16 @@ SET reset=[0m
 IF NOT EXIST .venv GOTO NO_VENV
 IF NOT EXIST .venv\Lib GOTO WRONG_VENV
 
+:: ENTERS VENV IF NOT ALREADY
+.venv\Scripts\activate
+
 :: INSTALLS TRDG IF NEEDEED
 IF EXIST .venv\Lib\site-packages\trdg GOTO TRDG_PRESENT
-.venv\Scripts\activate
 pip install trdg
 
 :: MOVES TEXT AND RUNS THE SCRIPT
 :TRDG_PRESENT
-XCOPY /s /i .venv\Lib\site-packages\trdg\fonts\latin .venv\Lib\site-packages\trdg\fonts\latin-backup
+XCOPY /s /i /Q .venv\Lib\site-packages\trdg\fonts\latin .venv\Lib\site-packages\trdg\fonts\latin-backup
 DEL /F /Q .venv\Lib\site-packages\trdg\fonts\latin
 COPY image_font.ttf .venv\Lib\site-packages\trdg\fonts\latin\font.ttf
 python3 generate_dataset.py
