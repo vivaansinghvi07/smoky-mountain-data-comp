bc3a7588c74c9bb4354fe38db4c541d211f5aad5
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-07-17 07:15:48 -0400

    edit

--- scripts/SAM.bat
+++ scripts/SAM.bat
@@ -1,5 +1,5 @@
 CD step2
 IF EXIST sam_vit_h_4b8939 GOTO END
-WGET https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+WGET https://dl.fbaipublicfiles.com/segment_anything/%1
 :END
 CD ..
--- scripts/SAM.sh
+++ scripts/SAM.sh
@@ -1,6 +1,6 @@
 cd step2
 if [ ! -f sam_vit_h_4b8939 ]
 then
-  wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+  wget https://dl.fbaipublicfiles.com/segment_anything/$1
 fi
 cd ..
