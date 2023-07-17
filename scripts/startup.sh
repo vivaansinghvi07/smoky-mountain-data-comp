4ff94e37afdfc13601c656856a1de5fd0d3e0864
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-07-17 13:01:20 -0400

    wrote startup script

---
+++ scripts/startup.sh
@@ -0,0 +1,9 @@
+# run basic startup scripts already written
+. scripts/venv.sh
+sh scripts/load_files.sh
+
+# get the files required for step 2
+cd step2
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+python3 load_onnx_model.py
+cd ..
