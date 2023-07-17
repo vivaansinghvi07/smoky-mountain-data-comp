1716133b27d848d54149cdea0b38580998a0b4dc
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-07-17 10:37:20 -0400

    removed undeed scripts

--- scripts/SAM.bat
+++
@@ -1,5 +0,0 @@
-CD step2
-IF EXIST sam_vit_h_4b8939 GOTO END
-WGET https://dl.fbaipublicfiles.com/segment_anything/%1
-:END
-CD ..
--- scripts/SAM.sh
+++
@@ -1,6 +0,0 @@
-cd step2
-if [ ! -f sam_vit_h_4b8939 ]
-then
-  wget https://dl.fbaipublicfiles.com/segment_anything/$1
-fi
-cd ..
